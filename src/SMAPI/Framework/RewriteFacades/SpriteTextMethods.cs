bac63a509995098098540f3ef6e44277a65a7ec3
ZaneYork <ZaneYork@qq.com>
2020-04-20 18:39:02 +0800

    Using rider as IDE and select VS2015 build tools

--- src/SMAPI/Framework/RewriteFacades/SpriteTextMethods.cs
+++ src/SMAPI/Framework/RewriteFacades/SpriteTextMethods.cs
@@ -9,12 +9,12 @@ namespace StardewModdingAPI.Framework.RewriteFacades
 {
     public class SpriteTextMethods : SpriteText
     {
-        public static int getWidthOfString(string s, int widthConstraint = 999999)
+        public new static int getWidthOfString(string s, int widthConstraint = 999999)
         {
             return SpriteText.getWidthOfString(s);
         }
 
-        public static void drawStringHorizontallyCenteredAt(SpriteBatch b, string s, int x, int y, int characterPosition = 999999, int width = -1, int height = 999999, float alpha = -1f, float layerDepth = 0.088f, bool junimoText = false, int color = -1, int maxWdith = 99999)
+        public new static void drawStringHorizontallyCenteredAt(SpriteBatch b, string s, int x, int y, int characterPosition = 999999, int width = -1, int height = 999999, float alpha = -1f, float layerDepth = 0.088f, bool junimoText = false, int color = -1, int maxWdith = 99999)
         {
             SpriteText.drawString(b, s, x - SpriteText.getWidthOfString(s) / 2, y, characterPosition, width, height, alpha, layerDepth, junimoText, -1, "", color);
         }
